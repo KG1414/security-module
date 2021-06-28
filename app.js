39543cf84c1dcf944732c9bdad604a9342f48d83
kylegallard <kylegallard@gmail.com>
2021-06-29 08:05:42 +1000

    Level 1 - Username and Password only

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,16 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -24,10 +35,37 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
 
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            };
+        };
+    });
+});
 
 app.listen(port, function () {
     console.log(`Server started on port:${port}`);
