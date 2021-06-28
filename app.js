f3cdfa7ab1ea43540f555097d33c2274e7419c81
kylegallard <kylegallard@gmail.com>
2021-06-28 19:45:50 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+// const bodyParser = require('body-parser');
+
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
+
+
+
+
+app.listen(port, function () {
+    console.log(`Server started on port:${port}`);
+});
